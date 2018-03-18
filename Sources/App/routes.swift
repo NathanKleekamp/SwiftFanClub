38ea4d1478f696e9a9a88b52571adba2dcfbce4b
Nathan Kleekamp <nkleekamp@gmail.com>
2018-03-18 09:17:46 -0400

    Added leaf config; setup basic routes for testing; added home.leaf

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,36 @@
 import Routing
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
+  router.get("setup") { req -> String in
+    let forum1 = Forum(id: 1, name: "Taylor's Songs")
+    let forum2 = Forum(id: 2, name: "Taylor's Albums")
+    let forum3 = Forum(id: 3, name: "Taylor's Concerts")
+
+    _ = forum1.create(on: req)
+    _ = forum2.create(on: req)
+    _ = forum3.create(on: req)
+
+    return "OK"
+  }
+
+  router.get { req -> Future<View> in
+    struct HomeContext: Codable {
+      var username: String?
+      var forums: [Forum]
     }
+
+    return Forum.query(on: req).all().flatMap(to: View.self) { forums in
+      let context = HomeContext(username: getUsername(req), forums: forums)
+      return try req.view().render("home", context)
+    }
+  }
+}
+
+func getUsername(_ req: Request) -> String? {
+  return "Testing"
 }
