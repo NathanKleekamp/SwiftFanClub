7f5b2e1b8fa595abd3185f7e4e3d790984a5e10b
Nathan Kleekamp <nkleekamp@gmail.com>
2018-03-18 08:58:18 -0400

    add db and migration configs

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,17 +1,31 @@
 import Vapor
+import Fluent
+import FluentSQLite
 
 /// Called before your application initializes.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
 public func configure(
-    _ config: inout Config,
-    _ env: inout Environment,
-    _ services: inout Services
+  _ config: inout Config,
+  _ env: inout Environment,
+  _ services: inout Services
 ) throws {
-    // Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
+  // Register routes to the router
+  let router = EngineRouter.default()
+  try routes(router)
+  services.register(router, as: Router.self)
 
-    // Configure the rest of your application here
+  let directoryConfig = DirectoryConfig.detect()
+  services.register(directoryConfig)
+
+  try services.register(FluentSQLiteProvider())
+
+  var databaseConfig = DatabaseConfig()
+  let db = try SQLiteDatabase(storage: .file(path: "\(directoryConfig.workDir)forums.db"))
+  databaseConfig.add(database: db, as: .sqlite)
+  services.register(databaseConfig)
+
+  var migrationConfig = MigrationConfig()
+  migrationConfig.add(model: Forum.self, database: .sqlite)
+  services.register(migrationConfig)
 }
